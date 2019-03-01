18a1b0f25ef022ba279686bb2eb3ab72a6823043
Anna Galvão <annaegp@hotmail.com>
2019-02-28 23:15:18 -0300

    implementando classes

---
+++ ClassSample/ClassSample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSample //unidade organizacional de classes
+{
+    class Program //classe basica. a classe representa diferentes estruturas - composta de metoros atributos e propriedades
+    {
+        static void Main(string[] args)
+        {
+            Aluno a1 = new Aluno(); // instanciando o objeto na memória
+            Aluno a2 = new Aluno();
+            Aluno a3 = new Aluno();
+            Aluno a4 = new Aluno();
+            Aluno a5 = new Aluno();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
